af5e27971f8be34fb6403b2bdae7437ecc13c54d
karyvdj <kaina51@hotmail.com>
2017-07-12 16:02:58 -0500

    Arroja resultados en consola

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
 var app = express();
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname +'/index.html');
-});
+app.use("/data", express.static(__dirname + '/data'));
 
-app.use("/static",express.static(path.join(__dirname,"data")));
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html")
+})
+
 app.listen(1234);
